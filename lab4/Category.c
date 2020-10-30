25d25fb3f41eb7d1b9f08f12b3b2a7c0ec973810
tpoulsen <t-poulsen@hotmail.com>
2020-10-29 21:03:43 -0600

    Initial commit for lab4. Almost the same as lab 3 except no arbitrary size limits on # of categories. some changes have been made, need to make a lot of fixes still

---
+++ lab4/Category.c
@@ -0,0 +1,143 @@
+//Author: Taylor Poulsen
+//Date: October 2020
+//Class: CS253
+//Professor: Jim Buffenbarger
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "Category.h"
+#include "error.h"
+#include "stringy.h"
+
+
+static Categories cats = 0;
+static int currentIndex = 0;
+static void rangeCount(Category* kitten, char target, char first, char last);
+static void foldedRangeCount(Category* kitten, char target, char first, char last);
+
+void addCategory(char* name, char* characters)
+{
+    Category newCategory;
+    newCategory.count = 0;
+    newCategory.name = name;
+    newCategory.chars = characters;
+
+
+    if (currentIndex == 0)         //array is empty
+    {
+        cats = (Categories)malloc(sizeof(*cats));
+        cats[currentIndex] = newCategory;
+        currentIndex++;
+    }
+    else if ((currentIndex & (currentIndex-1)) == 0)   //current index is a power of 2 meaning arraysize needs to be doubled
+    {
+        cats = (Categories)realloc(cats, sizeof(*cats)*2*currentIndex);
+        cats[currentIndex] = newCategory;
+        currentIndex++;
+    }
+    else                //array has an available index
+    {
+        cats[currentIndex] = newCategory;
+        currentIndex++;
+    }
+}
+
+void CharCatCount2(char target)
+{
+    for (int i = 0; cats[i].name; i++)
+    {
+        if (cats[i].chars[0] != '^')         //no capitalization folding
+        {    
+            for (int j = 0; cats[i].chars[j]; j++)
+                if (cats[i].chars[j] != '-') //no range identifier
+                {
+                    if (target == cats[i].chars[j])
+                        cats[i].count++;
+                }
+                else  //range identifier is detected:
+                    rangeCount(&cats[i], target, cats[i].chars[j-1], cats[i].chars[j+1]);  
+        }
+        else          //capitalization folding is detected:
+        {
+            for (int j = 1; cats[i].chars[j]; j++)
+            {
+                if (cats[i].chars[j] != '-') //no range identifier
+                {
+                    if (cats[i].chars[j] >= 'a' && cats[i].chars[j] <= 'z')       //if value is lowercase, check against both cases
+                    {
+                        if (target == cats[i].chars[j] || target == cats[i].chars[j]-('a'-'A'))
+                            cats[i].count++;
+                    }
+                    else if (cats[i].chars[j] >= 'A' && cats[i].chars[j] <= 'Z')  //if value is uppercase, check against both cases 
+                    {
+                        if (target == cats[i].chars[j] || target == cats[i].chars[j]+('a'-'A'))
+                            cats[i].count++;
+                    }
+                    else    //if value is not a letter, ignore folding, check like normal
+                        if (target == cats[i].chars[j])
+                            cats[i].count++;
+                }
+                else    //range identifier detected:
+                    foldedRangeCount(&cats[i], target, cats[i].chars[j-1], cats[i].chars[j+1]);
+            }
+        }
+    }
+}
+
+//check and count for characters in a given range (first char in range must be less than last char in range)
+void rangeCount(Category* kitten, char target, char first, char last)
+{
+    if (first < last)
+    {    
+        if (target < last && target > first)
+            kitten->count++;
+    }
+    else
+    {
+        fprintf(stderr, "Illegal range parameters: beginning of range [%d: \'%c\'] must be less than end of range [%d: \'%c\'].\n", (int)first, first, (int)last, last);
+        fprintf(stderr, "No counting was done on the range. Program integrity compromised. Please restart program.\n");
+        exit(1);
+    }
+}
+
+//check and count for characters in a given range with capitalization folding
+//first char in range must be less than last char in range  -  FOLDING ONLY WORKS FOR LETTERS
+void foldedRangeCount(Category* kitten, char target, char first, char last)
+{
+    if (first < last)
+    {
+        if (target >= 'a' && target <= 'z')         //target is lowercase, check against both cases
+        {
+            char fold = target - ('a'-'A');
+            if ((target > first && target < last) || (fold > first && fold < last))
+                kitten->count++;
+        }
+        else if (target >= 'A' && target <= 'Z')    //target is uppercase, check against both cases
+        {
+            char fold = target + ('a'-'A');
+            if ((target > first && target < last) || (fold > first && fold < last))
+                kitten->count++;
+        }
+        else        //target is not a letter. ignore folding, check like normal
+            if (target < first && target > last)
+                kitten->count++;
+    }
+    else
+    {
+        fprintf(stderr, "Illegal range parameters: beginning of range [%d: \'%c\'] must be less than end of range [%d: \'%c\'].\n", (int)first, first, (int)last, last);
+        fprintf(stderr, "No counting was done on the range. Program integrity compromised. Please restart program.\n");
+        exit(1);
+    } 
+}
+
+//create a neatly printable representation for the categories
+void printCategories()
+{
+    for (int i = 0; i < currentIndex; i++)
+        printf("%s : %d\n", cats[i].name, cats[i].count);
+}
+
+//------------------------------------------------------------------------------------------------
+//ask Jim about this print function and also about valgrind
+//--------------------------------------------------------------------------------------------------
